62f9e20f445bff6106289e51830a9a496f487def
bc.gahe <brayangamba15@gmail.com>
2026-07-12 21:59:17 -0500

    Serializers de usuario y registro

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -1,3 +1,31 @@
 from rest_framework import serializers
+from .models import User
 
-# Serializers de la app users
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id', 'username', 'email',
+            'first_name', 'last_name',
+            'role', 'phone', 'avatar',
+            'points', 'is_available',
+        ]
+        read_only_fields = ['id', 'points', 'role']
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'role', 'phone']
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+
+class PublicUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'is_available']
