735dc5eac72e33aa94e372107bf031ed87ed249f
bc.gahe <brayangamba15@gmail.com>
2026-07-13 20:19:27 -0500

    registrar rutas de notificaciones

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet
+from .notification_views import NotificationViewSet
 
 router = DefaultRouter()
+router.register(r'notifications', NotificationViewSet, basename='notifications')
 router.register(r'', UserViewSet, basename='users')
 
 urlpatterns = [
